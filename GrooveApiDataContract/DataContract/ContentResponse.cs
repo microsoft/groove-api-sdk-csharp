eb4b9ce58289679bbd4bc8b38659c757cf37e496
Bastien Teinturier <bastte@microsoft.com>
2016-09-13 10:56:33 +0200

    Adding data contract

---
+++ GrooveApiDataContract/DataContract/ContentResponse.cs
@@ -0,0 +1,36 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  
+//  All Rights Reserved.
+//  Licensed under the MIT License.
+//  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Groove.Api.DataContract
+{
+    using System.Runtime.Serialization;
+
+    [DataContract(Namespace = Constants.Xmlns)]
+    public class ContentResponse : BaseResponse
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public PaginatedList<Artist> Artists { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public PaginatedList<Album> Albums { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public PaginatedList<Track> Tracks { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public PaginatedList<Playlist> Playlists { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public PaginatedList<ContentItem> Results { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public GenreList Genres { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Culture { get; set; }
+    }
+}
---
+++ GrooveApiDataContract/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Microsoft.Groove.Api.DataContract")]
+[assembly: AssemblyDescription("Data contract for interacting with the Groove API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Microsoft.Groove.Api.DataContract")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
